b141d967e0febe355a311b3732f61e2e82c2f0b3
Goraniya Karan <karangoraniya@gmail.com>
2024-07-30 10:51:05 +0530

    feat : images & favicon added 🚀

---
+++ components/images/index.ts
@@ -0,0 +1,6 @@
+import lightPunk from "./lightPunk.jpg";
+import lightPunk2 from "./lightPunk2.jpg";
+import lightPunk3 from "./lightPunk3.jpg";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { lightPunk, lightPunk2, lightPunk3 };
