32cad912127a9ead92bfde8c7bd7d5d189b76704
Goraniya Karan <karangoraniya@gmail.com>
2024-07-30 10:51:22 +0530

    refactor: Refactor the design and move ABI to a separate file 🚀

---
+++ app/stake/abi.ts
@@ -0,0 +1,319 @@
+export const contractAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_token",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_NFTItem",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "target",
+        type: "address",
+      },
+    ],
+    name: "AddressEmptyCode",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "AddressInsufficientBalance",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "FailedInnerCall",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token",
+        type: "address",
+      },
+    ],
+    name: "SafeERC20FailedOperation",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "time",
+        type: "uint256",
+      },
+    ],
+    name: "Stake",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "time",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "rewardTokens",
+        type: "uint256",
+      },
+    ],
+    name: "UnStake",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "NFTItem",
+    outputs: [
+      {
+        internalType: "contract IERC1155",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes",
+      },
+    ],
+    name: "onERC1155BatchReceived",
+    outputs: [
+      {
+        internalType: "bytes4",
+        name: "",
+        type: "bytes4",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes",
+      },
+    ],
+    name: "onERC1155Received",
+    outputs: [
+      {
+        internalType: "bytes4",
+        name: "",
+        type: "bytes4",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "stakeNFT",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "stakes",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "supportsInterface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "unStakeNFT",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+export const nftABi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "setApprovalForAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,8 +1,13 @@
-'use client';
-import React, { useState } from 'react';
-import { useAccount, useSendTransaction } from 'wagmi';
-import { useWriteContract } from 'wagmi';
-import { toast, Toaster } from 'react-hot-toast';
+"use client";
+import React, { useState } from "react";
+import { useAccount, useSendTransaction } from "wagmi";
+import { useWriteContract } from "wagmi";
+import { toast, Toaster } from "react-hot-toast";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import images from "@/components/images/index";
+import { contractAbi, nftABi } from "./abi";
 
 const Mint = () => {
   const wallet = useAccount();
@@ -10,334 +15,16 @@ const Mint = () => {
   const { data: hash, writeContract } = useWriteContract();
   const [amtNft, setAmtNft] = useState(1);
 
-  const contractAddress = '0x8f3Ad8DEDD9AC6f683054af11aCD3BE96880De7d';
-  const nftAddr = '0x295718dfD46F2F88D63AA648Cb31dc67528eB960';
-  const contractAbi = [
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: '_token',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: '_NFTItem',
-          type: 'address',
-        },
-      ],
-      stateMutability: 'nonpayable',
-      type: 'constructor',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'target',
-          type: 'address',
-        },
-      ],
-      name: 'AddressEmptyCode',
-      type: 'error',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'account',
-          type: 'address',
-        },
-      ],
-      name: 'AddressInsufficientBalance',
-      type: 'error',
-    },
-    {
-      inputs: [],
-      name: 'FailedInnerCall',
-      type: 'error',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'token',
-          type: 'address',
-        },
-      ],
-      name: 'SafeERC20FailedOperation',
-      type: 'error',
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          internalType: 'address',
-          name: 'owner',
-          type: 'address',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'id',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'amount',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'time',
-          type: 'uint256',
-        },
-      ],
-      name: 'Stake',
-      type: 'event',
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          internalType: 'address',
-          name: 'owner',
-          type: 'address',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'id',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'amount',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'time',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'rewardTokens',
-          type: 'uint256',
-        },
-      ],
-      name: 'UnStake',
-      type: 'event',
-    },
-    {
-      inputs: [],
-      name: 'NFTItem',
-      outputs: [
-        {
-          internalType: 'contract IERC1155',
-          name: '',
-          type: 'address',
-        },
-      ],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: '',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: '',
-          type: 'address',
-        },
-        {
-          internalType: 'uint256[]',
-          name: '',
-          type: 'uint256[]',
-        },
-        {
-          internalType: 'uint256[]',
-          name: '',
-          type: 'uint256[]',
-        },
-        {
-          internalType: 'bytes',
-          name: '',
-          type: 'bytes',
-        },
-      ],
-      name: 'onERC1155BatchReceived',
-      outputs: [
-        {
-          internalType: 'bytes4',
-          name: '',
-          type: 'bytes4',
-        },
-      ],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: '',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: '',
-          type: 'address',
-        },
-        {
-          internalType: 'uint256',
-          name: '',
-          type: 'uint256',
-        },
-        {
-          internalType: 'uint256',
-          name: '',
-          type: 'uint256',
-        },
-        {
-          internalType: 'bytes',
-          name: '',
-          type: 'bytes',
-        },
-      ],
-      name: 'onERC1155Received',
-      outputs: [
-        {
-          internalType: 'bytes4',
-          name: '',
-          type: 'bytes4',
-        },
-      ],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'uint256',
-          name: '_tokenId',
-          type: 'uint256',
-        },
-        {
-          internalType: 'uint256',
-          name: '_amount',
-          type: 'uint256',
-        },
-      ],
-      name: 'stakeNFT',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: '',
-          type: 'address',
-        },
-      ],
-      name: 'stakes',
-      outputs: [
-        {
-          internalType: 'uint256',
-          name: 'tokenId',
-          type: 'uint256',
-        },
-        {
-          internalType: 'uint256',
-          name: 'amount',
-          type: 'uint256',
-        },
-        {
-          internalType: 'uint256',
-          name: 'timestamp',
-          type: 'uint256',
-        },
-      ],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'bytes4',
-          name: 'interfaceId',
-          type: 'bytes4',
-        },
-      ],
-      name: 'supportsInterface',
-      outputs: [
-        {
-          internalType: 'bool',
-          name: '',
-          type: 'bool',
-        },
-      ],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'uint256',
-          name: '_tokenId',
-          type: 'uint256',
-        },
-        {
-          internalType: 'uint256',
-          name: '_amount',
-          type: 'uint256',
-        },
-      ],
-      name: 'unStakeNFT',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-  ];
-  const nftABi = [
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'operator',
-          type: 'address',
-        },
-        {
-          internalType: 'bool',
-          name: 'approved',
-          type: 'bool',
-        },
-      ],
-      name: 'setApprovalForAll',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-  ];
+  const contractAddress = "0x8f3Ad8DEDD9AC6f683054af11aCD3BE96880De7d";
+  const nftAddr = "0x295718dfD46F2F88D63AA648Cb31dc67528eB960";
+
   const approvedNFt = async () => {
     try {
       const run = await writeContract({
         abi: nftABi,
         address: nftAddr,
-        functionName: 'setApprovalForAll',
-        args: ['0x8f3Ad8DEDD9AC6f683054af11aCD3BE96880De7d', true],
+        functionName: "setApprovalForAll",
+        args: ["0x8f3Ad8DEDD9AC6f683054af11aCD3BE96880De7d", true],
       });
       console.log(run);
       stakeNFT();
@@ -353,7 +40,7 @@ const Mint = () => {
       const run = await writeContract({
         abi: contractAbi,
         address: contractAddress,
-        functionName: 'stakeNFT',
+        functionName: "stakeNFT",
         args: [1, 4],
       });
       console.log(run);
@@ -366,56 +53,61 @@ const Mint = () => {
       const run = await writeContract({
         abi: contractAbi,
         address: contractAddress,
-        functionName: 'unStakeNFT',
+        functionName: "unStakeNFT",
         args: [1, amtNft],
       });
 
       console.log(run);
     } catch (error: any) {
-      console.log(error, 'dmeo');
+      console.log(error, "dmeo");
     }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-primary to-primary-foreground">
       <Toaster />
-
-      <div className="bg-white shadow-md rounded-lg p-8">
-        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">
-          NFT Staking
-        </h2>
-        <div className="mb-4">
-          <label
-            htmlFor="amount"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Amount
-          </label>
-          <input
-            type="number"
-            id="amount"
-            value={amtNft}
-            onChange={(e) => {
-              const value = Number(e.target.value);
-              setAmtNft(isNaN(value) ? 0 : value); // Set to 0 (or any default) if the input is not a valid number
-            }}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter amount"
+      <div className="bg-card rounded-xl shadow-lg p-8 w-full max-w-md">
+        <div className="flex flex-col items-center space-y-6">
+          <Image
+            src={images.lightPunk}
+            width={500}
+            height={500}
+            alt="NFT"
+            className="rounded-xl"
           />
-        </div>
-        <div className="flex justify-between gap-4">
-          <button
-            onClick={approvedNFt}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          >
-            Stake
-          </button>
-          <button
-            onClick={unstakeNFT}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          >
-            Unstake
-          </button>
+          <div className="flex flex-col items-center space-y-4 w-full">
+            <div className="flex items-center space-x-2 w-full">
+              <div className="flex flex-col items-start space-y-2 w-full">
+                <div className="text-sm font-medium text-muted-foreground">
+                  Amount
+                </div>
+                <Input
+                  type="number"
+                  placeholder="Stake your NFT"
+                  value={amtNft}
+                  onChange={(e) => {
+                    const value = Number(e.target.value);
+                    setAmtNft(isNaN(value) ? 0 : value);
+                  }}
+                  className="flex-1 bg-background border border-input rounded-md px-4 py-2 text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring"
+                />
+              </div>
+            </div>
+            <div className="flex items-center space-x-2 w-full">
+              <Button
+                onClick={approvedNFt}
+                className="bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring flex-1"
+              >
+                Stake
+              </Button>
+              <Button
+                onClick={unstakeNFT}
+                className="bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring flex-1"
+              >
+                Unstake
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
